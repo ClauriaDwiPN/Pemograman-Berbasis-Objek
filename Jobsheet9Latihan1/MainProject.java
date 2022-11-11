d316d6f124ed382e0fb1b07a1ec66f2828090df0
Clauria Dwi Putri Nabillah <92168645+ClauriaDwiPN@users.noreply.github.com>
2022-11-11 23:39:37 +0700

    Jobsheet 9

---
+++ Jobsheet9Latihan1/MainProject.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jobsheet9Latihan1;
+
+/**
+ *
+ * @author CLAURIA
+ */
+public class MainProject {
+    public static void main(String[] args) {
+        PerkalianKu objek = new PerkalianKu();
+        
+        objek.perkalian(25, 43);
+        objek.perkalian(34, 23, 56);
+    }
+}
---
+++ Jobsheet9Latihan1/PerkalianKu.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jobsheet9Latihan1;
+
+/**
+ *
+ * @author CLAURIA
+ */
+public class PerkalianKu {
+    void perkalian(int a, int b){
+        System.out.println(a * b);
+    }
+    void perkalian(int a, int b, int c){
+        System.out.println(a * b * c);
+    }
+}
